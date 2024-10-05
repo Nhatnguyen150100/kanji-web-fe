33b01934d06722326192a6d2000e51f5cb79f858
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2024-10-05 16:16:44 +0700

    fix code

--- src/components/layout/TheHeader.tsx
+++ src/components/layout/TheHeader.tsx
@@ -103,9 +103,9 @@ export default function TheHeader() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               <svg
@@ -115,9 +115,9 @@ export default function TheHeader() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 >
                   {' '}
                 </path>
@@ -133,6 +133,7 @@ export default function TheHeader() {
                 if (item?.subRoutes.length > 0) {
                   return (
                     <Popover
+                      key={item.name}
                       content={
                         <div className="flex flex-col justify-start items-center min-w-[80px]">
                           {item?.subRoutes.map((subRoute) => (
