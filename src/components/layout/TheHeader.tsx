b46bde655b81b10e61b3314a0448bb0cc7e9cb1c
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2024-10-22 16:16:13 +0700

    fix: header

--- src/components/layout/TheHeader.tsx
+++ src/components/layout/TheHeader.tsx
@@ -77,7 +77,7 @@ export default function TheHeader() {
 
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <nav className="bg-gray-800 border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <a href={DEFINE_ROUTERS.home} className="flex items-center">
             <img
@@ -85,13 +85,13 @@ export default function TheHeader() {
               className="mr-3 h-6 sm:h-9"
               alt="Flowbite Logo"
             />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
               KanjiWeb
             </span>
           </a>
           <div className="flex items-center lg:order-2">
             <Popover content={contentPopover} trigger="click">
-              <a className="hover:cursor-pointer text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+              <a className="hover:cursor-pointer text-white focus:ring-4 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-700 focus:outline-none focus:ring-gray-800">
                 <Avatar
                   className="me-3"
                   style={{ backgroundColor: '#00aaff' }}
@@ -103,7 +103,7 @@ export default function TheHeader() {
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2"
               aria-controls="mobile-menu-2"
               aria-expanded="false"
             >
@@ -176,7 +176,7 @@ export default function TheHeader() {
                             )
                               ? '!text-white font-bold'
                               : ''
-                          } lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700`}
+                          } lg:border-0 lg:hover:text-primary-700 lg:p-0 text-gray-400 lg:hover:text-white hover:bg-gray-700 hover:text-white lg:hover:bg-transparent border-gray-700`}
                         >
                           {item.name}
                         </Link>
@@ -192,7 +192,7 @@ export default function TheHeader() {
                         item.route === location.pathname
                           ? '!text-white font-bold'
                           : ''
-                      } lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700`}
+                      } lg:border-0 lg:hover:text-primary-700 lg:p-0 text-gray-400 lg:hover:text-white hover:bg-gray-700 hover:text-white lg:hover:bg-transparent border-gray-700`}
                     >
                       {item.name}
                     </Link>
